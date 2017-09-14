fd1ba806f4f5d25a56dbcefac238678b4b15fa64
React Conf Brasil <reactconfbr@gmail.com>
2017-09-14 02:30:42 -0300

    add globo, pipefy as new sponsors and adjust imgs

--- src/components/Sponsor.js
+++ src/components/Sponsor.js
@@ -7,6 +7,8 @@ import Text from './Text';
 import tallerImg from '../media/images/sponsors/taller.jpg';
 import quantoImg from '../media/images/sponsors/quanto.jpg';
 import fullcircleImg from '../media/images/sponsors/fullcircle.jpg';
+import globoImg from '../media/images/sponsors/globo.jpg';
+import pipefyImg from '../media/images/sponsors/pipefy.jpg';
 
 const styles = {
   container: css({
@@ -33,7 +35,15 @@ const styles = {
     borderRadius: 0,
     margin: 10,
     '> a > img': {
-      maxWidth: 200,
+      '.pleno': {
+        maxWidth: 300,
+        maxHeight: 100,
+      },
+      '.junior': {
+        maxWidth: 200,
+        maxHeight: 100,
+      },
+      maxWidth: 150,
       maxHeight: 100,
       margin: '0 auto',
       display: 'block',
@@ -50,7 +60,25 @@ const styles = {
   }),
 };
 
-const Sponsors = [
+const Plenos = [
+  {
+    id: 1,
+    name: 'Pipefy',
+    avatar: pipefyImg,
+    link: 'https://pipefy.com',
+  },
+];
+
+const Juniors = [
+  {
+    id: 2,
+    name: 'Globo.com',
+    avatar: globoImg,
+    link: 'http://www.globo.com',
+  },
+];
+
+const Padawans = [
   {
     id: 3,
     name: 'Taller',
@@ -76,19 +104,62 @@ class TextSponsor extends PureComponent {
     return (
       <div {...styles.container}>
         <Text title="Patrocínio" reverse />
+
         <div {...styles.cards}>
-          {Sponsors.map(sponsor => {
+          {Plenos.map(pleno => {
             return (
-              <div key={sponsor.id} {...styles.card}>
+              <div key={pleno.id} {...styles.card}>
                 <a
                   href={
-                    sponsor.link +
+                    pleno.link +
                     '?utm_source=reactconfbr-site&utm_medium=logo&utm_campaign=reactconfbr-2017'
                   }
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <img src={sponsor.avatar} alt={sponsor.name} />
+                  <img src={pleno.avatar} alt={pleno.name} className="pleno" />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+
+        <div {...styles.cards}>
+          {Juniors.map(junior => {
+            return (
+              <div key={junior.id} {...styles.card}>
+                <a
+                  href={
+                    junior.link +
+                    '?utm_source=reactconfbr-site&utm_medium=logo&utm_campaign=reactconfbr-2017'
+                  }
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src={junior.avatar}
+                    alt={junior.name}
+                    className="junior"
+                  />
+                </a>
+              </div>
+            );
+          })}
+        </div>
+
+        <div {...styles.cards}>
+          {Padawans.map(padawan => {
+            return (
+              <div key={padawan.id} {...styles.card}>
+                <a
+                  href={
+                    padawan.link +
+                    '?utm_source=reactconfbr-site&utm_medium=logo&utm_campaign=reactconfbr-2017'
+                  }
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img src={padawan.avatar} alt={padawan.name} />
                 </a>
               </div>
             );
