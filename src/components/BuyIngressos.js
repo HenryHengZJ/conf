e40a58274298771cb6250f5cee04296a6128ad10
Matheus Mesquita <matheusmesquita.168@gmail.com>
2017-09-20 21:34:44 -0300

    added buyIngressos' button disabled style (#76)

--- src/components/BuyIngressos.js
+++ src/components/BuyIngressos.js
@@ -28,8 +28,13 @@ const styles = {
     cursor: 'pointer',
     borderRadius: 40,
     textDecoration: 'none',
-    ':hover': {
+    ':not(.is-disabled):hover': {
       backgroundColor: Globals.colors.white,
+    },
+    '&.is-disabled': {
+      backgroundColor: Globals.colors.gray,
+      color: Globals.colors.lightGray,
+      cursor: 'initial',
     },
     '@media(max-width: 720px)': {
       display: 'block',
@@ -45,12 +50,11 @@ class TextBuyIngressos extends PureComponent {
         <Text title="07 Outubro 2017" subtitle="Sábado, das 8h às 18h">
           <br />
           <a
-            href="https://www.sympla.com.br/react-conf-brasil-2017__161239"
-            target="_blank"
+            className="is-disabled"
             rel="noopener noreferrer"
             {...styles.link}
           >
-            Ingressos Disponíveis
+            Ingressos Esgotados
           </a>
         </Text>
       </div>
