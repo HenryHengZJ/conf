17e713eb2c80b68aa152bf4fca31211efb08608b
Matheus Mesquita <matheusmesquita.168@gmail.com>
2017-09-27 08:16:50 -0300

    added João Gonçalves as Speaker (#83)

--- src/components/Speakers.js
+++ src/components/Speakers.js
@@ -19,6 +19,7 @@ import claraImg from '../media/images/speakers/clara-pb.jpg';
 import matheuslimaImg from '../media/images/speakers/matheuslima-pb.jpg';
 import keullerImg from '../media/images/speakers/keuller-pb.jpg';
 import haskellImg from '../media/images/speakers/haskell-pb.jpg';
+import joaoGoncalvesImg from '../media/images/speakers/joaogoncalves-pb.jpg';
 
 const styles = {
   container: css({
@@ -236,6 +237,16 @@ const Speakers = [
     avatar: haskellImg,
     worksIn: 'Rung',
     worksLink: 'https://app.rung.com.br/',
+  },
+  {
+    id: 16,
+    name: 'João Gonçalves',
+    theme: 'Show do Milhão React PWA (Caso de Sucesso)',
+    origin: 'Campinas, Brasil',
+    github: '#',
+    avatar: joaoGoncalvesImg,
+    worksIn: 'Movile',
+    worksLink: 'https://www.movile.com/#/',
   },
 ];
 
