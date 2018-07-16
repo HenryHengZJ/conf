7ba510dd5df898865b3da0e7f6e9a976a5b461f1
Daniel Galvão <daniel.galvao@vagas.com.br>
2018-07-16 15:16:19 -0300

    [Fix] Fix remove empty divs

--- src/components/Sponsor.js
+++ src/components/Sponsor.js
@@ -153,79 +153,87 @@ const Padawans2017 = [
 
 class TextSponsor extends PureComponent {
   render() {
-    return (
-      <div {...styles.container}>
-        {/*Senior &&
-          Senior.length &&
-          Plenos &&
-          Plenos.length &&
-          Juniors &&
-          Juniors.length &&
-          Padawans &&
-          Padawans.length &&
-          <Text title="Patrocínio" reverse />*/}
-        {Plenos.map(pleno => {
-          return (
-            <div {...styles.cards}>
-              <div key={pleno.id} {...styles.card}>
-                <a
-                  href={
-                    pleno.link +
-                    '?utm_source=reactconfbr-site&utm_medium=logo&utm_campaign=reactconfbr-2017'
-                  }
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={pleno.avatar} alt={pleno.name} className="pleno" />
-                </a>
-              </div>
-            </div>
-          );
-        })}
+    if (Plenos.length && Senior.length && Juniors.lenght && Padawans.length) {
+      return (
+        <div {...styles.container}>
+          {/*Senior &&
+            Senior.length &&
+            Plenos &&
+            Plenos.length &&
+            Juniors &&
+            Juniors.length &&
+            Padawans &&
+            Padawans.length &&
+            <Text title="Patrocínio" reverse />*/}
+          <div {...styles.cards}>
+            {Plenos.map(pleno => {
+              return (
+                <div key={pleno.id} {...styles.card}>
+                  <a
+                    href={
+                      pleno.link +
+                      '?utm_source=reactconfbr-site&utm_medium=logo&utm_campaign=reactconfbr-2017'
+                    }
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      src={pleno.avatar}
+                      alt={pleno.name}
+                      className="pleno"
+                    />
+                  </a>
+                </div>
+              );
+            })}
+          </div>
 
-        {Juniors.map(junior => {
-          return (
-            <div {...styles.cards}>
-              <div key={junior.id} {...styles.card}>
-                <a
-                  href={
-                    junior.link +
-                    '?utm_source=reactconfbr-site&utm_medium=logo&utm_campaign=reactconfbr-2017'
-                  }
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img
-                    src={junior.avatar}
-                    alt={junior.name}
-                    className="junior"
-                  />
-                </a>
-              </div>
-            </div>
-          );
-        })}
+          <div {...styles.cards}>
+            {Juniors.map(junior => {
+              return (
+                <div key={junior.id} {...styles.card}>
+                  <a
+                    href={
+                      junior.link +
+                      '?utm_source=reactconfbr-site&utm_medium=logo&utm_campaign=reactconfbr-2017'
+                    }
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img
+                      src={junior.avatar}
+                      alt={junior.name}
+                      className="junior"
+                    />
+                  </a>
+                </div>
+              );
+            })}
+          </div>
 
-        {Padawans.map(padawan => {
-          return (
-            <div {...styles.cards}>
-              <div key={padawan.id} {...styles.card}>
-                <a
-                  href={
-                    padawan.link +
-                    '?utm_source=reactconfbr-site&utm_medium=logo&utm_campaign=reactconfbr-2017'
-                  }
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={padawan.avatar} alt={padawan.name} />
-                </a>
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    );
+          <div {...styles.cards}>
+            {Padawans.map(padawan => {
+              return (
+                <div key={padawan.id} {...styles.card}>
+                  <a
+                    href={
+                      padawan.link +
+                      '?utm_source=reactconfbr-site&utm_medium=logo&utm_campaign=reactconfbr-2017'
+                    }
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <img src={padawan.avatar} alt={padawan.name} />
+                  </a>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      );
+    } else {
+      return false;
+    }
   }
 }
 
